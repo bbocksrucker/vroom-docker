4b0e7adca51affd1ea38791178d51d78f535ba2b
bbocksrucker <bernhard.bocksrucker@afreshed.at>
2025-06-07 19:35:03 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # copy the config.yml to the host if it doesn't exist yet
-# or copy it to the source if does exist
 if test -f /conf/config.yml; then
   cp /conf/config.yml /vroom-express/config.yml
 else
@@ -13,4 +12,5 @@ if ! test -f /conf/access.log; then
   touch /conf/access.log
 fi
 
-cd /vroom-express && VROOM_ROUTER=${VROOM_ROUTER} VROOM_LOG=${VROOM_LOG} exec npm start
+# 👉 Starte VROOM direkt (nicht npm/express)
+vroom -c /conf/config.yml --bind 0.0.0.0
